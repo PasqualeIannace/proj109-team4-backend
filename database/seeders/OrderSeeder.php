9f0bd97df457fbe54b77fc970838d1f8f07db876
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-02-22 17:39:13 +0100

    order model seeder migr

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Order;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $orders = config("order");
+        foreach($orders as $singleOrder)
+        {
+            $neworder=new Order();
+            $neworder->message=$singleOrder['message'];
+            $neworder->payment_status=$singleOrder['payment_status'];
+            $neworder->order_date=$singleOrder['order_date'];
+            $neworder->total_price=$singleOrder['total_price'];
+            $neworder->fill($singleOrder);
+            $neworder->save();
+        }
+    }
+}

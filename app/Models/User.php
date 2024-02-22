40f95bf49445d22c8f7e5d761485af260a40517d
israr <misrar09@gmail.com>
2024-02-22 17:29:54 +0100

    created Restaurant_type table + User_Restaurant_type table

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function types()
+    {
+        return $this->belongsToMany(Restaurant_type::class);
+    }
 }

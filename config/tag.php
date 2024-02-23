7a4ace2f5dd7af17bc13fca72db56a88e0df6d90
AbdullahAzza1 <abdullahazza141@gmail.com>
2024-02-23 10:44:27 +0100

    mod2

---
+++ config/tag.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    [
+        "email" => "hameked854@huizk.com"
+    ],
+    [
+        "password" => "xuNvr@8MpK!9phtvi6&8nX"
+    ],
+    [
+        "activity_name" => "King's Outlaw Tavern"
+    ],
+    [
+        "address" => "9869 Ryan Plaza Apt. 293"
+    ],
+    [
+        "VAT_number" => "77634180036"
+    ],
+
+];
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class User extends Seeder
+class Userseeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,7 +15,7 @@ class User extends Seeder
      */
     public function run()
     {
-
+        $users = config("user");
         foreach ($users as $user) {
             $new_user = new user();
             $new_user->email = $user["email"];
@@ -23,6 +24,14 @@ class User extends Seeder
             $new_user->address = $user["address"];
             $new_user->VAT_number = $user["VAT_number"];
             $new_user->save();
+        }
+
+        if (!User::where("email", "hameked854@huizk.com")->first()) {
+            $mainUser = new User();
+            $mainUser->name = "Ryan";
+            $mainUser->email = "hameked854@huizk.com";
+            $mainUser->password = ("xuNvr@8MpK!9phtvi6&8nX");
+            $mainUser->save();
         }
     }
 }

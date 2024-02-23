1935fc34772716318ba46cb35c12769fd3c6f606
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-02-23 17:57:09 +0100

    campi edit

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -5,7 +5,7 @@
 <h2 class="text-center">Modifica il piatto</h2>
 
 <div class="container-sm">
-    <form action="{{ route('admin.restaurants.update' , $editFood) }}" method="POST">
+    <form action="{{ route('admin.restaurants.update' , $editFood->id) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="row">
@@ -13,7 +13,7 @@
                 <div class="mb-3">
                     <label for="name" class="form-label">Nome Piatto</label>
                     <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
-                        value="{{ old('name')}} ">
+                        value="{{ old('name') ?? $editFood->name}} ">
                     @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -26,7 +26,7 @@
                 <div class="mb-3">
                     <label for="ingredients" class="form-label">Ingredienti</label>
                     <input type="text" class="form-control @error('ingredients') is-invalid @enderror" id="ingredients"
-                        name="ingredients" value="{{ old('ingredients')}} ">
+                        name="ingredients" value="{{ old('ingredients') ?? $editFood->ingredients}} ">
                     @error('ingredients')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -39,7 +39,7 @@
                 <div class="mb-3">
                     <label for="visible" class="form-label">Disponibile</label>
                     <input type="text" class="form-control @error('visible') is-invalid @enderror" id="visible"
-                        name="visible" value="{{ old('visible')}} ">
+                        name="visible" value="{{ old('visible')?? $editFood->visible}} ">
                     @error('visible')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -51,7 +51,7 @@
                     <div class="mb-3">
                         <label for="price" class="form-label">Prezzo</label>
                         <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
-                            name="price" value="{{ old('price')}} ">
+                            name="price" value="{{ old('price')?? $editFood->price}} ">
                         @error('price')
                         <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
@@ -65,7 +65,7 @@
                         <div class="mb-3">
                             <label for="description" class="form-label">Descrizione</label>
                             <input type="text" class="form-control @error('description') is-invalid @enderror"
-                                id="description" name="description" value="{{ old('description')}} ">
+                                id="description" name="description" value="{{ old('description') ?? $editFood->description}} ">
                             @error('description')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
@@ -77,7 +77,7 @@
                             <div class="mb-3">
                                 <label for="image" class="form-label">Immagine</label>
                                 <input type="text" class="form-control @error('image') is-invalid @enderror" id="image"
-                                    name="image" value="{{ old('image')}} ">
+                                    name="image" value="{{ old('image') ?? $editFood->image}} ">
                                 @error('image')
                                 <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror

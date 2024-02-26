3b311a128de5742e0f5cba58de428f65e413988a
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-02-26 17:23:16 +0100

    sintass

--- config/food_order.php
+++ config/food_order.php
@@ -35,37 +35,37 @@ return
         //order2
         [
             'food_id' => 2,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 2,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 3,
         ],
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 1,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 4,
         ],
         [
             'food_id' => 13,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 2,
         ],
         [
             'food_id' => 14,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 5,
         ],
         [
             'food_id' => 15,
-            'order_id' => 1,
+            'order_id' => 2,
             'quantity' => 3,
         ],
         //order3
@@ -92,303 +92,303 @@ return
         //order4
         [
             'food_id' => 3,
-            'order_id' => 1,
+            'order_id' => 4,
             'quantity' => 2,
         ],
         [
             'food_id' => 7,
-            'order_id' => 1,
+            'order_id' => 4,
             'quantity' => 1,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 4,
             'quantity' => 3,
         ],
         //order5
         [
             'food_id' => 2,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 2,
         ],
         [
             'food_id' => 5,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 1,
         ],
         [
             'food_id' => 8,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 3,
         ],
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 2,
         ],
         [
             'food_id' => 10,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 1,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 3,
         ],
         [
             'food_id' => 13,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 2,
         ],
         [
             'food_id' => 14,
-            'order_id' => 1,
+            'order_id' => 5,
             'quantity' => 1,
         ],
         //order6
         [
             'food_id' => 3,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 2,
         ],
         [
             'food_id' => 7,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 1,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 3,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 2,
         ],
         [
             'food_id' => 13,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 2,
         ],
         [
             'food_id' => 15,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 3,
         ],
         [
             'food_id' => 4,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 1,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 6,
             'quantity' => 2,
         ],
         //order7
         [
             'food_id' => 5,
-            'order_id' => 1,
+            'order_id' => 7,
             'quantity' => 1,
         ],
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 7,
             'quantity' => 2,
         ],
         [
             'food_id' => 10,
-            'order_id' => 1,
+            'order_id' => 7,
             'quantity' => 2,
         ],
         [
             'food_id' => 14,
-            'order_id' => 1,
+            'order_id' => 7,
             'quantity' => 3,
         ],
         //order8
         [
             'food_id' => 2,
-            'order_id' => 1,
+            'order_id' => 8,
             'quantity' => 2,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 8,
             'quantity' => 3,
         ],
         //order9
         [
             'food_id' => 5,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 3,
         ],
         [
             'food_id' => 8,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 2,
         ],
         [
             'food_id' => 10,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 4,
         ],
         [
             'food_id' => 13,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 1,
         ],
         [
             'food_id' => 15,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 5,
         ],
         [
             'food_id' => 4,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 2,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 9,
             'quantity' => 3,
         ],
         //order10
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 10,
             'quantity' => 4,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 10,
             'quantity' => 2,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 10,
             'quantity' => 1,
         ],
         [
             'food_id' => 14,
-            'order_id' => 1,
+            'order_id' => 10,
             'quantity' => 3,
         ],
         //order11
         [
             'food_id' => 3,
-            'order_id' => 1,
+            'order_id' => 11,
             'quantity' => 2,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 11,
             'quantity' => 3,
         ],
         //order12
         [
             'food_id' => 7,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 2,
         ],
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 3,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 1,
         ],
         [
             'food_id' => 13,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 2,
         ],
         [
             'food_id' => 2,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 1,
         ],
         [
             'food_id' => 5,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 2,
         ],
         [
             'food_id' => 8,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 3,
         ],
         [
             'food_id' => 10,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 1,
         ],
         [
             'food_id' => 15,
-            'order_id' => 1,
+            'order_id' => 12,
             'quantity' => 2,
         ],
         //order13
         [
             'food_id' => 3,
-            'order_id' => 1,
+            'order_id' => 13,
             'quantity' => 2,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 13,
             'quantity' => 3,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 13,
             'quantity' => 1,
         ],
         [
             'food_id' => 4,
-            'order_id' => 1,
+            'order_id' => 13,
             'quantity' => 2,
         ],
         [
             'food_id' => 1,
-            'order_id' => 1,
+            'order_id' => 13,
             'quantity' => 1,
         ],
         //order14
         [
             'food_id' => 3,
-            'order_id' => 1,
+            'order_id' => 14,
             'quantity' => 2,
         ],
         [
             'food_id' => 6,
-            'order_id' => 1,
+            'order_id' => 14,
             'quantity' => 1,
         ],
         [
             'food_id' => 12,
-            'order_id' => 1,
+            'order_id' => 14,
             'quantity' => 3,
         ],
         //order 15
         [
             'food_id' => 9,
-            'order_id' => 1,
+            'order_id' => 15,
             'quantity' => 2,
         ],
         [
             'food_id' => 10,
-            'order_id' => 1,
+            'order_id' => 15,
             'quantity' => 1,
         ],
         [
             'food_id' => 11,
-            'order_id' => 1,
+            'order_id' => 15,
             'quantity' => 3,
         ],    
 

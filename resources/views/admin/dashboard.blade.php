cddd190f8769f80ca573e314bfa11c4793e172eb
Thomas66 <thomassalvaterra2001@gmail.com>
2024-02-26 11:56:14 +0100

    change dashboard css

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,31 +2,14 @@
 
 @section('content')
 <div class="container-sm m-4 text-black">
-    {{-- <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div> --}}
     <div class="row justify-content-center">
-        <h2 class="text-center m-4">{{ $user->activity_name }}</h2>
-        {{-- {{ddd($user->activity_name);}} --}}
+        <h1 class="text-center m-4">{{ $user->activity_name }}</h2>
+            {{-- {{ddd($user->activity_name);}} --}}
     </div>
     <div class="d-flex justify-content-around">
         <div class="plateLf mybg">
             <p class="text-center fs-4">Il Tuo Menù</p>
-            <ol class="overflow-y-scroll h-75">
+            <ol class="overflow-y-scroll h-85 mt-5">
                 @foreach($foods as $food)
                 <li class="d-flex p-3">
                     <div class="small_img">
@@ -47,7 +30,7 @@
         <div class="statistic mt-6">
             <p class="text-center fs-4">Le tue Statistiche</p>
             <img src="https://s.tmimgcdn.com/scr/1200x750/137800/elementi-di-infografica-statistica-economica-del-grafico_137852-original.jpg"
-                class="w-100">
+                class="w-100 d-flex align-items-center">
         </div>
     </div>
 

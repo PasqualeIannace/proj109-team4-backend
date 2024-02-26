8a9abbd98433218d34a3811a20b1608a8b995dc5
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-26 17:18:53 +0100

    Pasquale seeder

--- config/food_order.php
+++ config/food_order.php
@@ -6,4 +6,345 @@ return
             'order_id' => 1,
             'quantity' => 3,
         ],
+        // PASQUALE FOODS
+        [
+            'food_id' => 64,
+            'order_id' => 2,
+            'quantity' => 3,
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 52,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 78,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 48,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 67,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 82,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 59,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 71,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 56,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 63,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 47,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 59,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 80,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 61,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 77,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 55,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 48,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 79,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 53,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 69,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 84,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 57,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 73,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 65,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 81,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 50,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 70,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 62,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 78,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 56,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 49,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 64,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 83,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 72,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 51,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 68,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 85,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 60,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 76,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 52,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 72,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 83,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 63,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 58,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 74,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 47,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 79,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 60,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 54,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 80,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 70,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 53,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 64,
+            "order_id" => 75,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 85,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 57,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 46,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 69,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 82,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 56,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 72,
+            "order_id" => 49,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 65,
+            "order_id" => 73,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 68,
+            "order_id" => 64,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 70,
+            "order_id" => 81,
+            "quantity" => 3
+        ],
+        [
+            "food_id" => 67,
+            "order_id" => 55,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 66,
+            "order_id" => 77,
+            "quantity" => 4
+        ],
+        [
+            "food_id" => 73,
+            "order_id" => 50,
+            "quantity" => 1
+        ],
+        [
+            "food_id" => 69,
+            "order_id" => 71,
+            "quantity" => 2
+        ],
+        [
+            "food_id" => 71,
+            "order_id" => 62,
+            "quantity" => 1
+        ],
     ];

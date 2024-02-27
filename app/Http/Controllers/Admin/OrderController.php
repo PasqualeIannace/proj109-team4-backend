ed86ed48db32fb62e6428e7b828272b1a9afa880
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-27 18:40:50 +0100

    Add bugged order show

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -34,4 +34,21 @@ class OrderController extends Controller
 
         return view("admin.orders.index", compact("orders"));
     }
+
+    public function show(Order $order)
+    {
+        // $userId = Auth::id();
+
+
+        // $orders = Order::whereHas('foods', function ($query) use ($userId) {
+        //     $query->where('user_id', $userId)
+        //         ->withTrashed(); // Include soft-deleted foods
+        // })->with(['foods' => function ($query) use ($userId) {
+        //     $query->where('user_id', $userId)
+        //         ->withTrashed() // Include soft-deleted foods
+        //         ->withPivot('quantity');
+        // }])->get();
+
+        return view("admin.orders.show", compact("order"));
+    }
 }

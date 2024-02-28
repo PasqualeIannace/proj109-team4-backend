2ebf7ec6ee61173505f3a71347b7630e02aba64f
Thomas66 <thomassalvaterra2001@gmail.com>
2024-02-28 18:00:52 +0100

    create show details

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -2,38 +2,58 @@
 
 @section('content')
 
-<div class="container">
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h2>ATTENZIONE! DATI A CAZZO DI CANE - È RICHIESTO UN FIX</h2>
-    <h1>Elenco degli ordini</h1>
+<div class="container mybg p-5 mt-5 rounded">
+    <header class="d-flex justify-content-between mt-4 mb-4 text-white">
+        <h2>Deliveboo</h2>
+        <h2>Conferma d'ordine</h2>
+    </header>
 
-    <table class="table">
+    <table class="table mt-5 mb-5">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Data Ordine</th>
-                <th>Quantità</th>
-                <!-- Aggiungi altre colonne secondo necessità -->
+                <th scope="col">Destinatario</th>
+                <th scope="col">Indirizzo di consegna</th>
+                <th scope="col">Contatto telefonico</th>
+                <th scope="col">Email</th>
+                <th scope="col">Data di consegna</th>
+                <th scope="col">Messaggio rilasciato (facoltativo)</th>
             </tr>
         </thead>
         <tbody>
+            <tr>
+                <td>{{ $order->name_surname }}</td>
+                <td>{{ $order->address }}</td>
+                <td>{{ $order->phone_number }}</td>
+                <td>{{ $order->email }}</td>
+                <td>{{ $order->order_date }}</td>
+                <td>{{ $order->message }}</td>
+            </tr>
+        </tbody>
+    </table>
 
+    <div class="text-white">
+        <div>Gentile cliente {{ $order->name_surname }},</div>
+        <div>la ringraziamo per aver effettuato l'ordine presso il nostro ristorante. Ci auguriamo che il nostro pasto
+            sia
+            stato apprezzato.</div>
+        <div>Saluti dallo staff!</div>
+    </div>
+
+
+
+    <table class="table mt-5 mb-5">
+        <thead>
+            <tr>
+                <th scope="col">Numero dell'ordine</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Quantità</th>
+                <th scope="col">Prezzo al pezzo</th>
+                <th scope="col">Prezzo totale</th>
+            </tr>
+        </thead>
+        <tbody>
             <tr>
                 <td>{{ $order->id }}</td>
-                <td>{{ $order->order_date }}</td> <!-- Assumo che 'order_date' sia un campo della tua tabella Order -->
                 <td>
                     <ul>
                         @foreach ($order->foods as $food)
@@ -41,10 +61,37 @@
                         @endforeach
                     </ul>
                 </td>
-                <!-- Aggiungi altre colonne secondo necessità -->
+                <td>
+                    <ul>
+                        @foreach ($order->foods as $food)
+                        <li>{{ $food->pivot->quantity }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        @foreach ($order->foods as $food)
+                        <li>€ {{ $food->price }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td></td>
             </tr>
         </tbody>
     </table>
+
+    <hr class="text-white">
+    <div class="text-white">
+        <h3>Prezzo totale da pagare: €</h3>
+        <div>
+            @if ($order->payment_status = 1)
+            Il pagamento è avvenuto con successo
+            @else
+            Il pagamento deve ancora essere effettuato
+            @endif
+        </div>
+    </div>
+
 </div>
 
 @endsection

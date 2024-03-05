7d29d25f3bd1b41910263030e9ea8343176c75ba
PasqualeIannace <pasquale.iannace@gmail.com>
2024-03-05 17:30:18 +0100

    Add types with users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -9,44 +10,26 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-       // $users = User::all();
-        //Recupera l'ID dell'utente autenticato
-        // $userId = Auth::id();
-        // //Recupera solo i Food collegati all'utente autenticato
-        // $foods = Food::where('user_id', $userId)
-        //     //->where('visible', true)
-        //     ->get();
-        // $data = [
-        //     "success" => true,
-        //     "payload" => $users
-        // ];
-        // return response()->json($data);
-        // $restaurantTypes = Restaurant_type::with('users')->get();
-        // $data = [
-        //     "success" => true,
-        //     "payload" => $restaurantTypes
-        // ];
-
-        // Recupera tutti gli utenti
-    
-
-    // Applica il filtro se sono stati passati parametri
-    if ($request->filled('selectedTypes')) {
-        $selectedTypes = $request->input('selectedTypes');
-        $users->whereHas('types', function ($query) use ($selectedTypes) {
-            $query->whereIn('id', $selectedTypes);
-        });
-    } else {
         $users = User::query();
-    }
 
-    // Ottieni gli utenti filtrati
-    $filteredUsers = $users->get();
+        // Carica i dati della relazione 'types' (restaurant_types)
+        $users->with('types');
 
-    $data = [
-        "success" => true,
-        "payload" => $filteredUsers
-    ];
+        // Applica il filtro se sono stati passati parametri
+        if ($request->filled('selectedTypes')) {
+            $selectedTypes = $request->input('selectedTypes');
+            $users->whereHas('types', function ($query) use ($selectedTypes) {
+                $query->whereIn('id', $selectedTypes);
+            });
+        }
+
+        // Ottieni gli utenti filtrati con i dati della relazione 'types'
+        $filteredUsers = $users->get();
+
+        $data = [
+            "success" => true,
+            "payload" => $filteredUsers
+        ];
 
         return response()->json($data);
     }

a0bb15e7338102f94f66e373fecaa8932cd3b2bb
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-03-05 15:55:26 +0100

    usercontroller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,19 +7,47 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::all();
+       // $users = User::all();
         //Recupera l'ID dell'utente autenticato
         // $userId = Auth::id();
         // //Recupera solo i Food collegati all'utente autenticato
         // $foods = Food::where('user_id', $userId)
         //     //->where('visible', true)
         //     ->get();
-        $data = [
-            "success" => true,
-            "payload" => $users
-        ];
+        // $data = [
+        //     "success" => true,
+        //     "payload" => $users
+        // ];
+        // return response()->json($data);
+        // $restaurantTypes = Restaurant_type::with('users')->get();
+        // $data = [
+        //     "success" => true,
+        //     "payload" => $restaurantTypes
+        // ];
+
+        // Recupera tutti gli utenti
+    
+
+    // Applica il filtro se sono stati passati parametri
+    if ($request->filled('selectedTypes')) {
+        $selectedTypes = $request->input('selectedTypes');
+        $users->whereHas('types', function ($query) use ($selectedTypes) {
+            $query->whereIn('id', $selectedTypes);
+        });
+    } else {
+        $users = User::query();
+    }
+
+    // Ottieni gli utenti filtrati
+    $filteredUsers = $users->get();
+
+    $data = [
+        "success" => true,
+        "payload" => $filteredUsers
+    ];
+
         return response()->json($data);
     }
 

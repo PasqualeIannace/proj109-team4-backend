eb6b48099643d217477a9f50b8f43b85623acdc7
Thomas66 <thomassalvaterra2001@gmail.com>
2024-03-06 13:04:46 +0100

    create order request and validation for Foods

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\ValidFood;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OrderRequest extends FormRequest
@@ -25,7 +26,10 @@ class OrderRequest extends FormRequest
     {
         return [
             'token' => 'required',
-            'amount' => 'required'
+            'food' => [
+                'required',
+                new ValidFood()
+            ]
         ];
     }
 }

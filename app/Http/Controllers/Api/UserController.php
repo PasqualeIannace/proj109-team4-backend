a9cb42b963c24d3c936a6e3f183f44dbc04c77c8
misrar09 <misrar09@gmail.com>
2024-03-11 14:30:43 +0100

    inserted user into user detail

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
 
         if ($request->filled('selectedTypes')) {
             $selectedTypes = $request->input('selectedTypes');
-        
+
             $users->where(function ($query) use ($selectedTypes) {
                 foreach ($selectedTypes as $type) {
                     $query->whereHas('types', function ($q) use ($type) {
@@ -48,4 +48,25 @@ class UserController extends Controller
     //         "payload" => $event ? $event : "Nessun evento corrispondente all'id"
     //     ]);
     // }
+
+    public function show($id)
+    {
+        try {
+            // Find the user by ID
+            $user = User::findOrFail($id);
+
+            // Return the user details
+            return response()->json([
+                'success' => true,
+                'payload' => $user,
+                'logo_activity' => $user->logo_activity,
+            ], 200);
+        } catch (\Exception $e) {
+            // Handle the exception and return an error response
+            return response()->json([
+                'success' => false,
+                'message' => 'Error fetching user: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
 }
